b495650f89ac4ce002380753340b3bbc5a35ce48
katsu <nieznanm@gmail.com>
2020-05-06 19:48:00 +0200

    Add url to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="take_photo",
-    version="1.0.0",
+    version="1.0.1",
     author="Michał Nieznański",
     author_email="nieznanm@gmail.com",
     description="Take a photo with a webcam",
+    url="https://github.com/m-nez/take_photo",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
